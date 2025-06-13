ef05920fa41a63dc156f75b3fcfd2489bc00cd5b
Thomas Tucker <thtucker@gmail.com>
2025-06-13 12:23:19 -0500

    Update emailer.py

--- emailer.py
+++ emailer.py
@@ -1,14 +1,19 @@
 import smtplib
 from email.message import EmailMessage
 
-def send_email(filepath):
+def send_email(filepath, user, password):
     msg = EmailMessage()
     msg["Subject"] = "Weekly Zillow Report"
-    msg["From"] = "thtucker@gmail.com"
-    msg["To"] = "thtucker@gmail.com"
+    msg["From"] = user
+    msg["To"] = user
     msg.set_content("Attached is your weekly Zillow report.")
     with open(filepath, "rb") as f:
-        msg.add_attachment(f.read(), maintype="application", subtype="octet-stream", filename="zillow_report.xlsx")
+          msg.add_attachment(
+            f.read(),
+            maintype="application",
+            subtype="octet-stream",
+            filename="zillow_report.xlsx"
+        )
     with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
-        smtp.login("thtucker@gmail.com", "your_app_password_here")
+        smtp.login(user, password)
         smtp.send_message(msg)
