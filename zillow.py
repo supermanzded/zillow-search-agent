0d3f0d018db14280443aa2c021a6e0713c219dfe
Thomas Tucker <thtucker@gmail.com>
2025-06-14 15:51:17 -0500

    Update zillow.py

--- zillow.py
+++ zillow.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from bs4 import BeautifulSoup
 from webdriver_manager.chrome import ChromeDriverManager
 import time
