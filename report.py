56e875ef93484823af2400a40910dcdcdf75c617
Thomas Tucker <thtucker@gmail.com>
2025-06-14 14:50:37 -0500

    Update report.py

--- report.py
+++ report.py
@@ -2,6 +2,6 @@ import pandas as pd
 
 def generate_excel_report(data):
     df = pd.DataFrame(data)
-    filepath = "/mnt/data/zillow_report.xlsx"
+    filepath = "zillow_report.xlsx"
     df.to_excel(filepath, index=False)
     return filepath
